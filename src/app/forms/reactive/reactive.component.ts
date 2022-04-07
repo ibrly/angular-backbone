ec79e2f27a84e53b259f8c4ece247b61283319ee
Ebrahim Soliman <ebrahimsoliman@Ebrahims-MacBook-Pro.local>
2022-04-08 01:41:49 +0200

    finish reactive forms

--- src/app/forms/reactive/reactive.component.ts
+++ src/app/forms/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import {AsyncValidatorFn, FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-reactive',
@@ -10,6 +11,7 @@ export class ReactiveComponent implements OnInit {
   binding: string = 'default';
   genders: Array<string> = ['Male', 'Female']
   signupForm: FormGroup;
+  forbiddenNamesList: Array<string> = ['Mo', 'Re']
 
   constructor() {
   }
@@ -17,12 +19,18 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        first_name: new FormControl(null, [Validators.required]),
-        last_name: new FormControl(null, [Validators.required]),
+        first_name: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        last_name: new FormControl(null, [Validators.required], this.forbiddenLastNames as AsyncValidatorFn),
       }),
       gender: new FormControl('Male', [Validators.required]),
       hobbies: new FormArray([])
     })
+    this.signupForm.valueChanges.subscribe((value => {
+      console.log(value)
+    }))
+    this.signupForm.statusChanges.subscribe((status => {
+      console.log(status)
+    }))
   }
 
   onAddHobby() {
@@ -37,5 +45,47 @@ export class ReactiveComponent implements OnInit {
   onSubmit() {
     console.log(this.signupForm.get('first_name'))
     console.log(this.signupForm)
+  }
+
+  forbiddenNames(control: FormControl): { [p: string]: boolean } | null {
+    if (this.forbiddenNamesList.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true}
+    }
+    return null;
+  }
+
+  fill() {
+    this.signupForm.setValue({
+      userData: {
+        first_name: 'Ebrahim', last_name: 'Ahmed'
+      }, gender: 'Male',
+      hobbies: []
+    })
+  }
+
+  patch() {
+    this.signupForm.patchValue({
+      userData: {
+        last_name: 'Soliman'
+      }
+    })
+  }
+
+  reset() {
+    this.signupForm.reset()
+  }
+
+  forbiddenLastNames(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === '') {
+          resolve(null)
+        } else if (control.value === 'SSS') {
+          resolve({'lastNameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
   }
 }
