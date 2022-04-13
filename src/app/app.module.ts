dd9a5ef1c8d2a2721095438f24300156b42fc1d9
Ibrahim Aly <40615465+ibrly@users.noreply.github.com>
2022-04-13 04:34:20 +0200

    service worker PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {SharedModule} from "./shared/shared.module";
 import {MeetupsComponent} from './meetups/meetups.component';
 import {reducer} from "./store/reducers/meetup/meetups.reducer";
 import {MeetupsEffects} from "./store/reducers/meetup/effects/meetups.effects";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,12 @@ import {MeetupsEffects} from "./store/reducers/meetup/effects/meetups.effects";
     HttpClientModule,
     FormsModule,
     SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
