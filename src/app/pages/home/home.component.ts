18f79b078506fdce92b1a6f43248f01a41aed350
Devlopers <mennabaghsh@Devloperss-Mac-mini.local>
2022-04-28 14:09:16 +0200

    s

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  grid = 6
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  handleSize(event: any) {
+    if (event.target.innerWidth < 700) {
+      this.grid = 1
+    } else {
+      this.grid = 6
+    }
+  }
 }
