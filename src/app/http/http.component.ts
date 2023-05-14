31b079ffd2d0895cac1725854c06b97225c24cd2
ebrahimahmeddlivr <ebrahimahmed97090@gmail.com>
2023-05-14 08:22:28 +0300

    ci

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-http',
   templateUrl: './http.component.html',
-  styleUrls: ['./http.component.scss']
 })
 export class HttpComponent {
 
